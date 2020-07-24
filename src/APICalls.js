2f271e0afc3f0944f5fbd7fc75cb36e0e3aad0f2
Jakub Hlavačka <43497822+JKBGIT1@users.noreply.github.com>
2020-07-24 13:19:09 +0200

    City, Restaurants and Categories API calls

--- src/APICalls.js
+++ src/APICalls.js
@@ -1,25 +1,41 @@
-export const getCity = (inputCity) => {
-    fetch(`https://developers.zomato.com/api/v2.1/cities?q=${inputCity}`, {
+export const getCity = async (inputCity) => {
+    const response = await fetch(`https://developers.zomato.com/api/v2.1/cities?q=${inputCity}`, {
         method: 'get',
         headers: new Headers({
             "user-key": "596aefd8ba1b3c415c0ef3fc3523449b",
             "content-type": "application/json"
         }),
-    })
-        .then(response => response.json())
-        .then(data => console.log(data.location_suggestions[0].id))
-        .catch(error => console.log(error));
+    });
 
-    // fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&entity_type=city`)
+    const data = await response.json();
+
+    if (data.location_suggestions.length !== 0) // ak sa najde mesto, ktore chcel pouzivatel vyhladat, tak funkcia vrati jeho id
+        return data.location_suggestions[0].id; // vrati Id mesta, ktore som chcel vyhladat
+    else // ked sa data o meste nenajdu, tak vrati null
+        return null;
 }
 
-// export const getCategories = () => {
-//     fetch('https://developers.zomato.com/api/v2.1/categories', {
-//         method: 'get',
-//         headers: new Headers({
-//             "user-key": "596aefd8ba1b3c415c0ef3fc3523449b",
-//             "content-type": "application/json"
-//         }),
-//     }).then(response => response.json())
-//         .then(data => console.log(data));
-// }
+export const getRestaurants = async (cityId) => {
+    const response = await fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&entity_type=city`, {
+        method: 'get',
+        headers: new Headers({
+            "user-key": "596aefd8ba1b3c415c0ef3fc3523449b",
+            "content-type": "application/json"
+        }),
+    });
+
+    return await response.json();
+}
+
+export const getCategories = async () => {
+    const response = await fetch('https://developers.zomato.com/api/v2.1/categories', {
+        method: 'get',
+        headers: new Headers({
+            "user-key": "596aefd8ba1b3c415c0ef3fc3523449b",
+            "content-type": "application/json"
+        }),
+    });
+
+    const data = await response.json()
+    return data.categories
+}
--- src/Contexts/Context.js
+++ src/Contexts/Context.js
@@ -1,5 +1,5 @@
 import React, { createContext } from "react";
-import {getCity} from "../APICalls";
+import {getCity, getRestaurants, getCategories} from "../APICalls";
 
 export const Context = createContext(); // Potrebne pre vyuzivanie Context API
 
@@ -7,11 +7,15 @@ class ContextProvider extends React.Component {
     state = {
         screenWidth: window.innerWidth, // componenty budu podla sirky obrazovky v providerovi renderovat rozne elementy
         searchingCity: null, // sem sa ulozi id adresa vyhladavaneho mesta z API callu
+        restaurantsApi: null, // sem ulozim vyhladane restauracie z api
+        categoriesApi: null, // sem ulozim vsetky kategorie jedal, ktore mozu poskytovat restuaracie
         inputText: "", // nazov mesta, ktory sa po kliknuti na hladat ikonu bude vyhladavat v Zomato API
     }
     // aby som mal vzdy aktualnu sirku obrazovky, tak musim po nacitani componentu nadstavit tento EventListener
-    componentDidMount() {
+    async componentDidMount() {
         window.addEventListener('resize', this.screenWidthChanged);
+        const categories = await getCategories(); // zo Zomato API nataham vsetky kategorie jedal, ktore mozu mat restauracie
+        this.setState({categoriesApi: categories}); // ulozim GET call z API do stavu, aby som s nim mohol dalej pracovat
     }
     // po tom ako sa component prestane zobrazovat, tak odoberiem EventListener
     componentWillUnmount() {
@@ -20,7 +24,16 @@ class ContextProvider extends React.Component {
     // tato funckia zmeni v this.state hodnotu screenWidth, podla aktualnej sirky obrazovky
     screenWidthChanged = () => this.setState({screenWidth: window.innerWidth});
     // vyhlada sa id mesta, ktoreho nazov sa nachadza v this.state.inputText
-    getRestaurantsByCity = () => getCity(this.state.inputText);
+    getRestaurantsByCity = async () => {
+        let cityId = await getCity(this.state.inputText); // na zaklade nazvu mesta vo vyhladavani najdem jeho ID v API
+
+        if (cityId) { // ak sa naslo ID mesta, tak vyhladam v API vsetky restauracie, ktore sa v nom nachadzaju
+            let result = await getRestaurants(cityId);
+            this.setState({restaurantsApi: result}); // vyhladavanie restauracii ulozim do stavu, preto s nim budem dalej pracovat
+            result.restaurants.map(restaurant => console.log(restaurant.restaurant)); // sluzi aktualne iba na kontrolu
+        } else
+            alert("Udaje o tomto meste nie su dostupne.");
+    };
     // vzdy ked sa zmeni hodnota v inpute pre search, tak sa zmeni hodnota v this.state.inputText
     changeInputText = (event) => this.setState({inputText: event.target.value});
 
