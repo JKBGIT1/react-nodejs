2d168c1cf7f0b6f961ed6b161c7550b7782b8e6e
Jakub Hlavačka <43497822+JKBGIT1@users.noreply.github.com>
2020-07-29 17:18:24 +0200

    Restaurants and Categories

--- src/Components/Main/SearchMain.js
+++ src/Components/Main/SearchMain.js
@@ -2,11 +2,18 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import {Context} from "../../Contexts/Context";
 import Typography from "@material-ui/core/Typography";
-import Paper from "@material-ui/core/Paper";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import ListSubheader from "@material-ui/core/ListSubheader";
 import GridListTileBar from "@material-ui/core/GridListTileBar";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import Checkbox from "@material-ui/core/Checkbox";
+import ListItemText from "@material-ui/core/ListItemText";
+import NoImage from "../../Images/no_image.jpg"
 
 class SearchMain extends React.Component {
     state = {
@@ -14,6 +21,12 @@ class SearchMain extends React.Component {
             noResults: {
                 margin: "2%",
                 marginTop: "75px",
+            },
+            noImage: {
+                backgroundImg: `url(${NoImage})`,
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+                backgroundSize: "cover",
             }
         }
     }
@@ -24,32 +37,45 @@ class SearchMain extends React.Component {
                 return (
                     <Grid container style={{marginTop: "75px"}}>
                         <Grid item md={2}>
-                            <Paper>
-                                {context.categoriesApi.map((categorie) => (
-                                    <Typography>{categorie.categories.name}</Typography>
-                                ))}
-                            </Paper>
+                            <Card>
+                                <CardHeader subheader="Categories"/>
+                                <List dense component="div" role="list">
+                                    {context.categoriesApi.map((categorie) => (
+                                        <ListItem key={categorie.categories.id} role="listitem" button>
+                                            <ListItemIcon>
+                                                <Checkbox
+                                                    checked={false}
+                                                    tabIndex={-1}
+                                                    disableRipple
+                                                />
+                                            </ListItemIcon>
+                                            <ListItemText id={categorie.categories.id} primary={categorie.categories.name} />
+                                        </ListItem>
+                                    ))}
+                                </List>
+                            </Card>
                         </Grid>
                         <Grid item container md={10}>
+                            <Card>
                             <GridList cellHeight={360}>
-                                <GridListTile key="Subheader" cols={2} style={{ height: 'auto', backgroundColor: "darkgrey" }}>
+                                <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
                                     <ListSubheader component="div">Restaurants in {context.inputText}</ListSubheader>
                                 </GridListTile>
                                 {context.restaurantsApi.restaurants.map((res) => (
                                     <GridListTile key={res.restaurant.id}>
-                                        <img src={res.restaurant.featured_image} alt={"Restaurant Image"} />
+                                        <img
+                                            style={this.state.styles.noImage}
+                                            src={res.restaurant.featured_image}
+                                            alt={""}
+                                        />
                                         <GridListTileBar
                                             title={res.restaurant.name}
-                                            subtitle={<p>
-                                                        City: {res.restaurant.location.city}
-                                                        Locality: {res.restaurant.location.locality}
-                                                        Address: {res.restaurant.location.address}
-                                                      </p>
-                                            }
+                                            subtitle={<span>{res.restaurant.location.address}</span>}
                                         />
                                     </GridListTile>
                                 ))}
                             </GridList>
+                            </Card>
                         </Grid>
                     </Grid>
                 );
