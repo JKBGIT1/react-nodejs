2e50e3ec5b31c0cfa7755792435755aaeef5f43a
Jakub Hlavačka <43497822+JKBGIT1@users.noreply.github.com>
2020-08-04 12:34:33 +0200

    Added try catch to API calls

--- src/APICalls.js
+++ src/APICalls.js
@@ -1,41 +1,56 @@
 export const getCity = async (inputCity) => {
-    const response = await fetch(`https://developers.zomato.com/api/v2.1/cities?q=${inputCity}`, {
-        method: 'get',
-        headers: new Headers({
-            "user-key": "596aefd8ba1b3c415c0ef3fc3523449b",
-            "content-type": "application/json"
-        }),
-    });
+    try {
+        const response = await fetch(`https://developers.zomato.com/api/v2.1/cities?q=${inputCity}`, {
+            method: 'get',
+            headers: new Headers({
+                "user-key": "596aefd8ba1b3c415c0ef3fc3523449b",
+                "content-type": "application/json"
+            }),
+        });
 
-    const data = await response.json();
+        const data = await response.json();
 
-    if (data.location_suggestions.length !== 0) // ak sa najde mesto, ktore chcel pouzivatel vyhladat, tak funkcia vrati jeho id
-        return data.location_suggestions[0].id; // vrati Id mesta, ktore som chcel vyhladat
-    else // ked sa data o meste nenajdu, tak vrati null
+        if (data.location_suggestions.length !== 0) // ak sa najde mesto, ktore chcel pouzivatel vyhladat, tak funkcia vrati jeho id
+            return data.location_suggestions[0].id; // vrati Id mesta, ktore som chcel vyhladat
+        else // ked sa data o meste nenajdu, tak vrati null
+            return null;
+    } catch (error) {
+        console.log(error);
         return null;
+    }
 }
 
 export const getRestaurants = async (cityId, start) => {
-    const response = await fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&entity_type=city&start=${start}&count=100`, {
-        method: 'get',
-        headers: new Headers({
-            "user-key": "596aefd8ba1b3c415c0ef3fc3523449b",
-            "content-type": "application/json"
-        }),
-    });
+    try {
+        const response = await fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&entity_type=city&start=${start}&count=100`, {
+            method: 'get',
+            headers: new Headers({
+                "user-key": "596aefd8ba1b3c415c0ef3fc3523449b",
+                "content-type": "application/json"
+            }),
+        });
 
-    const data = await response.json()
-    return data.restaurants.filter((res) => res.restaurant.featured_image !== ""); // vrati len restauracie, ktore maju featured_image
+        const data = await response.json()
+        return data.restaurants.filter((res) => res.restaurant.featured_image !== ""); // vrati len restauracie, ktore maju featured_image
+    } catch (error){
+        console.log(error);
+        return null;
+    }
 }
 
 export const getRestaurantDetail = async (restaurantId) => {
-    const response = await fetch(`https://developers.zomato.com/api/v2.1/restaurant?res_id=${restaurantId}`, {
-        method: 'get',
-        headers: new Headers({
-            "user-key": "596aefd8ba1b3c415c0ef3fc3523449b",
-            "content-type": "application/json"
-        }),
-    });
+    try {
+        const response = await fetch(`https://developers.zomato.com/api/v2.1/restaurant?res_id=${restaurantId}`, {
+            method: 'get',
+            headers: new Headers({
+                "user-key": "596aefd8ba1b3c415c0ef3fc3523449b",
+                "content-type": "application/json"
+            }),
+        });
 
-    return response.json();
+        return response.json();
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
 }
